2d82239bbe4f6777b62006700a0d3c77249165b1
hakimihamzan <hakimihamzan20@gmail.com>
2025-10-12 22:49:09 +0800

    Refactor main.py to add command-line argument handling and remove hardcoded prompt

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <prompt>")
+        sys.exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     
-    print("Hello from gemini-cli!", api_key)
     client = genai.Client(api_key=api_key)
     
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents=sys.argv[1]
     )
     
     print(response.text)
